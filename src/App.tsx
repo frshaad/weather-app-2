09bd1d346c94490d762e8c855f84f970c8dad4d2
Farshad <farshad.hatami95@gmail.com>
2023-02-27 23:11:31 +0330

    chore: project base structure initialized

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Routes>
+      <div>
+        <h1 className='uppercase'>Hello World!</h1>
+      </div>
+    </Routes>
+  );
+};
+
+export default App;
